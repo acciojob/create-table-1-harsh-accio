c1b76c4bfd141ce4694f6fe9cb8d0617f323d955
harsh-accio <rajputharsh9897@gmail.com>
2025-09-22 12:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function insert_Row() {
-    //Write your code here
-	const table = document.getElementById('sampleTable');
-	const tr = document.createElement('tr');
-	tr.innerHTML=`
-	<td>New Cell1</td>
-	<td>New Cell2</td>
-	`;
-	table.insertBefore(tr,table.rows[0]);
+    const table = document.getElementById('sampleTable');
+    const tbody = table.tBodies[0] || table; // get tbody if exists
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>New Cell1</td>
+        <td>New Cell2</td>
+    `;
+    // Insert at top
+    tbody.insertBefore(tr, tbody.firstElementChild);
 }
